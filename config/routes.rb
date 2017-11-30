e2d6ecad86154ebb56ec39b4f6ffd1d278055668
AbbyDC <chub_009@yahoo.com>
2017-11-30 14:46:24 +0800

    edited html for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :landing
-  root 'landing#index'
+  root 'landing#admin_home'
 end
