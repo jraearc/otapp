231378b79d05146477c7866ad87eacddd70e0caf
AbbyDC <chub_009@yahoo.com>
2017-12-03 16:11:15 +0800

    new folder per view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :landing
-  root 'landing#admin_settings'
+  root 'landing#index'
 end
