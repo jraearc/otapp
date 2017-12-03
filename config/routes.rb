feb3270a4f5df671ef4ae59ba0a34a7c1f80097f
Rich Yap <richsmyap@gmail.com>
2017-12-03 23:27:11 +0800

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,16 @@ Rails.application.routes.draw do
 
   # Defines admin pages
   get 'admin/', to: 'admin#admin_home'
-  get 'admin/home', to: 'admin#admin_home'
-  get 'admin/settings', to: 'admin#admin_settings'
-  get 'admin/app_profile', to: 'admin#admin_app_profile'
-  get 'admin/courses', to: 'admin#admin_courses'
+  get 'admin/home', to: 'admin#admin_home', as: 'admin_home'
+  get 'admin/settings', to: 'admin#admin_settings', as: 'admin_settings'
+  get 'admin/app_profile', to: 'admin#admin_app_profile', as: 'admin_app_profile'
+  get 'admin/courses', to: 'admin#admin_courses', as: 'admin_courses'
 
   # Defines student pages
   get 'student/', to: 'student#student_home'
-  get 'student/home', to: 'student#student_home'
-  get 'student/settings', to: 'student#student_settings'
-  get 'student/profile', to: 'student#student_profile'
-  get 'student/school_view', to: 'student#school_view'
+  get 'student/home', to: 'student#student_home', as: 'student_home'
+  get 'student/settings', to: 'student#student_settings', as: 'student_settings'
+  get 'student/profile', to: 'student#student_profile', as: 'student_profile'
+  get 'student/school_view', to: 'student#school_view', as: 'student_school_view'
 
 end
