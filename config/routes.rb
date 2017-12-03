f0f80f91d82a8060f2e3233d3180777c9f98a180
Jahziel Rae Arceo <rae.arceo@live.com>
2017-12-03 20:47:19 +0800

    Slight change in admin and home landing

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   # Defines admin pages
+  get 'admin/', to: 'admin#admin_home'
   get 'admin/home', to: 'admin#admin_home'
   get 'admin/settings', to: 'admin#admin_settings'
   get 'admin/app_profile', to: 'admin#admin_app_profile'
   get 'admin/courses', to: 'admin#admin_courses'
 
   # Defines student pages
+  get 'student/', to: 'student#student_home'
   get 'student/home', to: 'student#student_home'
   get 'student/settings', to: 'student#student_settings'
   get 'student/profile', to: 'student#student_profile'
