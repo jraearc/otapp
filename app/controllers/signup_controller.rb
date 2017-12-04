c3661f0a75361c2b64244bf41dd909c6f3bc0dbb
Jahziel Rae Arceo <rae.arceo@live.com>
2017-12-05 06:16:42 +0800

    Finalized signup create

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,23 +1,34 @@
 class SignupController < ApplicationController
 
   def create
-    @user = User.new(username: params[:username], name: params[:name], e_mail: params[:e_mail], password: params[:password])
-    if params[:type].eql? "Admin"
-    	@admin = Admin.new(admin_userid: @user.userid, school_pos: params[:position])
-    elsif params[:type].eql? "Student"
-    	@student = Student.new(student_userid: @user.userid, birthdate: params[:birthdate], address: params[:address], civil_status: params[:civil_status])
-    end
-    if @user.save
-	    cookies.signed[:username] = @user.username
-	  	if params[:type].eql? "Student"
-		 	redirect_to student_home_path
-		elsif params[:type].eql? "Admin"
-			redirect_to admin_home_path
-		end
-	else
-      	flash[:error] = "Unable to create new account."
+  	begin
+	    @user = User.new(:username => params[:username], :name => params[:name], :e_mail => params[:e_mail], :password => params[:password])
+	    if @user.save
+	    	if params[:type].eql? "Admin"
+		    	@admin = Admin.new(:admin_userid => @user.userid, :school_pos => params[:school_pos])
+		    	@school = School.find_by(sname: params[:school_name])
+		    	@manage = Manage.new(:admin_userid => @admin.admin_userid, :school_id => @school.school_id)
+		    elsif params[:type].eql? "Student"
+		    	@student = Student.new(:student_userid => @user.userid, :birthdate => params[:birthdate], :address => params[:address], :civil_status => params[:civil_status])
+		    end
+		    cookies.signed[:username] = @user.username
+		    cookies[:userid] = @user.userid
+		  	if params[:type].eql? "Student"
+		  		@student.save
+			 	redirect_to student_home_path
+			elsif params[:type].eql? "Admin"
+				@admin.save
+				@manage.save
+				redirect_to admin_home_path
+			end
+		else
+	      	flash[:error] = "Unable to create new account."
+			redirect_to root_url
+	    end
+	rescue
+		flash[:error] = "Unable to create new account."
 		redirect_to root_url
-    end
+	end
   end
 
 end
