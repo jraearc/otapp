c715ec3d058388ab2e4c6debbb2423b415320266
AbbyDC <chub_009@yahoo.com>
2017-12-04 17:15:15 +0800

    student controller

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -4,6 +4,8 @@ def student_home
 end
 def student_profile
 	@apps = Apply.joins('join applications on applies.ref_no = applications.ref_no join courses on courses.course_id = applications.course_id join schools on applies.school_id = schools.school_id').select('applies.student_userid, schools.school_id, schools.sname, applies.is_received, courses.course_name')
+
 	#need pa ng "where" clause kasi lahat lang ng inaapplyan ng student yung lalabas
+	# @apps = Apply.joins('join applications on applies.ref_no = applications.ref_no join courses on courses.course_id = applications.course_id join schools on applies.school_id = schools.school_id').select('applies.student_userid, schools.school_id, schools.sname, applies.is_received, courses.course_name').where(student_userid:1)
 end
 end
