558457a72b187f6849070a8e04f5649a67ad3394
Jahziel Rae Arceo <rae.arceo@live.com>
2017-12-04 17:34:59 +0800

    added new route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'student/home', to: 'student#student_home', as: 'student_home'
   get 'student/settings', to: 'student#student_settings', as: 'student_settings'
   get 'student/profile', to: 'student#student_profile', as: 'student_profile'
+  get 'student/profile/:id', to: 'student#student_profile_id', as: 'student_profile_id'
   get 'student/school_view', to: 'student#school_view', as: 'student_school_view'
 
 end
