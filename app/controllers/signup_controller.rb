c1a713d0f205c334afc37e09d15163d8438332c8
Jahziel Rae Arceo <rae.arceo@live.com>
2017-12-05 04:21:18 +0800

    Fixing heroku app error

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -2,15 +2,15 @@ class SignupController < ApplicationController
 
   def create
     @user = User.new(username: params[:username], name: params[:name], e_mail: params[:e_mail], password: params[:password])
-    if params[:type] == "Admin"
+    if params[:type].eql? "Admin"
     	@admin = Admin.new(admin_userid: @user.userid, school_pos: params[:position])
-    elsif params[:type] == "Student"
+    elsif params[:type].eql? "Student"
     	@student = Student.new(student_userid: @user.userid, birthdate: params[:birthdate], address: params[:address], civil_status: params[:civil_status])
     if @user.save
 	    cookies.signed[:username] = @user.username
-	  	if params[:type] == "Student"
+	  	if params[:type].eql? "Student"
 		 	redirect_to student_home_path
-		elsif params[:type] == "Admin"
+		elsif params[:type].eql? "Admin"
 			redirect_to admin_home_path
 		end
 	else
