7fd5fa751008ec79b9f3c3fbb0725f7a9e51fb43
Jahziel Rae Arceo <rae.arceo@live.com>
2017-12-05 01:25:18 +0800

    CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   helper_method :account_signed_in?, :current_account
 
