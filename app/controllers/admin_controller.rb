13714e617ba25ef8cbcd47dd1d392819ea8fb5bd
AbbyDC <chub_009@yahoo.com>
2017-12-04 16:18:15 +0800

    controller edit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,5 @@
 class AdminController < ApplicationController
+def admin_courses
+	@all_courses = Course.order(:course_name)
+end
 end
--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,2 +1,5 @@
 class StudentController < ApplicationController
+def student_home
+	@all_schools = School.order(:sname)
+end
 end
