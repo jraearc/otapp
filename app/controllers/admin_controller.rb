64ea8257b6d4ca1fec5a46a8e9c3e80901257c06
AbbyDC <chub_009@yahoo.com>
2017-12-04 17:33:16 +0800

    courses offered

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
 def admin_courses
 	@all_courses = Course.order(:course_name)
+	@courses_offered = Offer.joins('join courses on offers.course_offered_id = courses.course_id').select('courses.course_name, courses.sector, courses.tuition_fee').where(school_id:1)
+	#Palitan pa yung school id, testing lang yan haha
 end
 end
