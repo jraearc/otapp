d3372a69090f5e9d5a09397158abc573efaf10a1
AbbyDC <chub_009@yahoo.com>
2017-12-04 23:57:38 +0800

    admin select statements

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,23 @@
 class AdminController < ApplicationController
 def admin_courses
 	@all_courses = Course.order(:course_name)
-	@courses_offered = Offer.joins('join courses on offers.course_offered_id = courses.course_id').select('courses.course_name, courses.sector, courses.tuition_fee').where(school_id:1)
+	@school = School.find(1) #change constant to session id
+	@courses_offered = Offer.joins('join courses on offers.course_offered_id = courses.course_id').select('courses.course_name, courses.sector, courses.tuition_fee').where(school_id:1) #change constant to session id
 	#Palitan pa yung school id, testing lang yan haha
 end
 def admin_home
-	@applicants = Apply.joins('join users on applies.student_userid = users.userid join applications on applies.ref_no = applications.ref_no join courses on applications.course_id = courses.course_id join schools on applies.school_id = schools.school_id').select('users.name, schools.sname').where("schools.sname='Academy Asia School of Technology and the Arts, Inc.'")
+	@school = School.find(1) #change constant to session id
+	@school_admin = Admin.find(1) #change constant to session id
+	@applicants = Apply.joins('join users on applies.student_userid = users.userid join applications on applies.ref_no = applications.ref_no join courses on applications.course_id = courses.course_id join schools on applies.school_id = schools.school_id').select('users.userid,users.name, schools.sname').where("schools.sname='Academy Asia School of Technology and the Arts, Inc.'")
 end
 def admin_app_profile
-	@apps = Apply.joins('join applications on applies.ref_no = applications.ref_no join courses on courses.course_id = applications.course_id join schools on applies.school_id = schools.school_id').select('applies.is_received, courses.course_name')
+	@applicant = User.find(params[:id]) #galing yung params sa pinasa from link ng admin_home
+	@app_deets = Student.find(params[:id])
+	@apps = Apply.joins('join applications on applies.ref_no = applications.ref_no join courses on courses.course_id = applications.course_id join schools on applies.school_id = schools.school_id').select('applies.is_received, courses.course_name').where(student_userid:params[:id])
 	#need pa ng "where" clause kasi lahat lang ng inaapplyan ng student yung lalabas
+end
+def admin_settings
+	@school = School.find(1) #change constant to session id
+	@school_admin = Admin.find(1) #change constant to session id
 end
 end
